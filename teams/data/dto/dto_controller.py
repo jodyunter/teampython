4c2e17638d63520c7ccde93ba4d37773224fce80
Jody Unterschutz <jody.u01@gmail.com>
2020-12-13 15:52:50 -0700

    Working an an app service to control the overall flow and processes of the app

--- teams/data/dto/dto_controller.py
+++ teams/data/dto/dto_controller.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
 
 from teams.data.dto.dto_base import Base
 from teams.domain.controller import Controller
@@ -7,8 +7,9 @@ from teams.domain.controller import Controller
 class ControllerDTO(Base, Controller):
     __tablename__ = "controller"
 
+    oid = Column(String, primary_key=True)
     current_day = Column(Integer)
     current_year = Column(Integer)
 
     def __init__(self, controller):
-        Controller.__init__(self, controller.current_year, controller.current_day)
+        Controller.__init__(self, oid, controller.current_year, controller.current_day)
--- teams/domain/controller.py
+++ teams/domain/controller.py
@@ -1,5 +1,6 @@
 class Controller:
 
-    def __init__(self, current_year, current_day):
+    def __init__(self, oid, current_year, current_day):
+        self.oid = oid
         self.current_year = current_year
         self.current_day = current_day
