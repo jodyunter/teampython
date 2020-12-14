55ab13608382667ee0f5c62a8204a11e12773884
Jody Unterschutz <jody.u01@gmail.com>
2020-12-13 21:26:10 -0700

    playing

--- teams/ConsoleUI/data_setup.py
+++ teams/ConsoleUI/data_setup.py
@@ -5,7 +5,7 @@ from teams.services.team_service import TeamService
 
 team_name_list = ["Toronto", "Vancouver", "Ottawa", "Montreal", "Quebec City", "Winnipeg",
                   "Calgary", "Edmonton", "Saskatoon", "Victoria", "Halifax", "Regina", "Hamilton",
-                  "London", "St John's"]
+                  "London", "St John's", "Kelowna"]
 # team_name_list = ["Toronto", "Vancouver", "Ottawa"]
 
 
