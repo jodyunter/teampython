6e608b1423ec7bbb28730536fbcbb8f00fb777e7
jodyu <jody_unterschutz@tcenergy.com>
2020-12-16 11:04:07 -0700

    working a better scoring system

--- teams/domain/game.py
+++ teams/domain/game.py
@@ -18,19 +18,48 @@ class Game:
         self.rules = rules
         self.processed = processed
 
+    # one way that may save processing power is to have all matrices from -20 to +20 created by hand or in properties
+    skill_value = 1000
+    score_matrix = [[0, 0, 7500], [1, 7501, 32501], [2, 32502, 67502], [3, 67503, 92503], [4, 92504, 112504],
+                    [5, 112505, 127505], [6, 127506, 140006], [7, 140007, 141007], [8, 141008, 141508],
+                    [9, 141509, 141559], [10, 141560, 141585], [11, 141586, 141606], [12, 141607, 141617],
+                    [13, 141618, 141623], [14, 141624, 141627], [15, 141628, 141629]]
+    score_increments = [7500, 25000, 35000, 25000, 20000, 15000, 12500, 1000, 500, 50, 25, 20, 10, 5, 3, 1]
+
+    change_matrix = [-10, -5, 0, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10, 10]
+    max_skill = 15
+
+    def get_score(self, skill_diff, random):
+        copy_inc = self.score_increments.copy()
+        score_matrix = []
+        for i in range(self.max_skill):
+            multiplier = (1 + self.change_matrix[a[0]] / 100 * skill_diff)
+
+        copy_matrix = self.score_matrix[:]
+        for a in copy_matrix:
+            multiplier = (1 + self.change_matrix[a[0]] / 100 * skill_diff)
+            a[1] = int(a[1] * multiplier)
+            a[2] = int(a[2] * multiplier)
+
+            print(copy_matrix[a[0]])
+            print(self.score_matrix[a[0]])
+
+        random_value = random.randint(copy_matrix[0][1], copy_matrix[self.max_skill][2])
+
+        for a in copy_matrix:
+            possible_score = a[0]
+            range_low = a[1]
+            range_high = a[2]
+
+            if range_low <= random_value <= range_high:
+                return possible_score
+
     def play(self, random):
         if not self.complete:
-            diff = self.home_team.skill - self.away_team.skill
-            diff = int(diff)
+            skill_diff = self.home_team.skill - self.away_team.skill
 
-            if diff > 10:
-                diff = 10
-
-            if diff < -10:
-                diff = -10
-
-            self.home_score = random.randint(0, 20 + diff)
-            self.away_score = random.randint(0, 20 - diff)
+            self.home_score = self.get_score(skill_diff, random)
+            self.away_score = self.get_score(skill_diff, random)
             while self.home_score == self.away_score and not self.rules.can_tie:
                 a = random.randint(-6, 6)
                 if a < 0:
@@ -39,4 +68,3 @@ class Game:
                     self.home_score += 1
 
             self.complete = True
-
