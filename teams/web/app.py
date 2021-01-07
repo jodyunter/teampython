12ee3316ad2b7a794c458322a28d480c8b9473bb
jodyu <jody_unterschutz@tcenergy.com>
2021-01-07 10:44:27 -0700

    Got the flask server running with the database.  Still need to work on how to configure everything properly

--- teams/web/app.py
+++ teams/web/app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from markupsafe import escape
 
+from teams.data.database import Database
+from teams.services.team_service import TeamService
+
 app = Flask(__name__)
+
+Database.init_db("sqlite:///C:\\temp\\sqlite\\Data\\mydb.db")
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    team_service = TeamService()
+    result = ""
+    for t in team_service.get_all():
+        result += t.name + "\n"
+
+    return 'Hello World\n' + result
 
 
 @app.route('/user/<username>')
