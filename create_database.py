f89ad955520c90fd0949b10cbbbdffea6b17b2be
Jody Unterschutz <jody.u01@gmail.com>
2021-01-21 12:27:24 -0700

    cleaning up for ease of use

--- create_database.py
+++ create_database.py
@@ -1,16 +1,23 @@
-from app_config import db_connection_string
 from teams.data.database import Database
 from alembic.config import Config
 from alembic import command
 
+alembic_file = "alembic.ini"
+alembic_section = "alembic_section"
+conn_string = "db_string"
+revision_name = "head"
+
 configs = {
-    "sqlite_local": "sqlite:///C:\\temp\\team_data.db",
-    "postres_local": "postgresql://test_user:password@localhost/testdb"
+    "sqlite_local": {alembic_section: "sqlite_local", conn_string: "sqlite:///C:\\temp\\team_data.db"},
+    "postres_local": {alembic_section: "postres_local",
+                      conn_string: "postgresql://test_user:password@localhost/testdb"}
+
 }
 
-config = "sqlite_local"
+config_name = "sqlite_local"
 
-Database.init_db(configs[config])
 
-alembic_cfg = Config("alembic.ini", config)
-command.stamp(alembic_cfg, "head")
+Database.init_db(configs[config_name][conn_string])
+
+alembic_cfg = Config(alembic_file, configs[config_name][alembic_section])
+command.stamp(alembic_cfg, revision_name)
