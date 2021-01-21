0d5705c8755c665a8ae516bd80b060e7744a13ee
Jody Unterschutz <jody.u01@gmail.com>
2021-01-21 12:39:32 -0700

    getting closer to being elastic beanstalk ready

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ Database.init_db(db_connection_string)
 #  logging.basicConfig(filename=log_file, filemode='w', format=log_format, level=log_level, datefmt=log_date_format)
 logging.basicConfig(format=log_format, level=log_level, datefmt=log_date_format)
 
-rounds = 2
+rounds = 1
 do_home_and_away = False
 rules_name = "Season"  # other is Playoff
 
