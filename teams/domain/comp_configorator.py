ec602fe95ef5ebde944e46497df5aa2705858f2c
Jody Unterschutz <jody.u01@gmail.com>
2021-02-23 20:19:08 -0700

    Good example of playoff by config done.

--- teams/domain/comp_configorator.py
+++ teams/domain/comp_configorator.py
@@ -62,41 +62,42 @@ class CompetitionConfigurator:
 
     @staticmethod
     def create_competition_group(competition_group_config, current_groups, competition):
-        if competition is None:
-            raise DomainError("Competition has to exist before the groups can be setup.")
+        if competition_group_config is not None:
+            if competition is None:
+                raise DomainError("Competition has to exist before the groups can be setup.")
 
-        current_group_list = [cg for cg in current_groups if cg.name == competition_group_config.name]
-        if current_group_list is not None and len(current_group_list) == 1:
-            return current_group_list[0]
+            current_group_list = [cg for cg in current_groups if cg.name == competition_group_config.name]
+            if current_group_list is not None and len(current_group_list) == 1:
+                return current_group_list[0]
 
-        parent_group = None
+            parent_group = None
 
-        if competition_group_config.parent_group_configuration is not None:
-            parent_group = [pg for pg in current_groups if
-                            pg.name == competition_group_config.parent_group_configuration.name]
-            if len(parent_group) == 0:
-                # parent group is not setup
-                parent_group = CompetitionConfigurator.create_competition_group(
-                    competition_group_config.parent_group_configuration, current_groups, competition)
-            else:
-                parent_group = parent_group[0]
+            if competition_group_config.parent_group_configuration is not None:
+                parent_group = [pg for pg in current_groups if
+                                pg.name == competition_group_config.parent_group_configuration.name]
+                if len(parent_group) == 0:
+                    # parent group is not setup
+                    parent_group = CompetitionConfigurator.create_competition_group(
+                        competition_group_config.parent_group_configuration, current_groups, competition)
+                else:
+                    parent_group = parent_group[0]
 
-        sub_competition = None
+            sub_competition = None
 
-        if competition_group_config.sub_competition_configuration is not None:
-            sub_competition = [sb for sb in competition.sub_competitions if
-                               sb.name == competition_group_config.sub_competition_configuration.name]
-            if sub_competition is None or len(sub_competition) == 0:
-                raise DomainError("You are setting up groups before sub competitions.")
-            sub_competition = sub_competition[0]
+            if competition_group_config.sub_competition_configuration is not None:
+                sub_competition = [sb for sb in competition.sub_competitions if
+                                   sb.name == competition_group_config.sub_competition_configuration.name]
+                if sub_competition is None or len(sub_competition) == 0:
+                    raise DomainError("You are setting up groups before sub competitions.")
+                sub_competition = sub_competition[0]
 
-        new_group = CompetitionGroup(competition_group_config.name,
-                                     parent_group, sub_competition,
-                                     [], competition_group_config.group_type)
+            new_group = CompetitionGroup(competition_group_config.name,
+                                         parent_group, sub_competition,
+                                         [], competition_group_config.group_type)
 
-        current_groups.append(new_group)
+            current_groups.append(new_group)
 
-        return new_group
+            return new_group
 
     @staticmethod
     def process_competition_team_configuration(team_configuration, current_groups, current_teams, competition):
@@ -160,8 +161,6 @@ class CompetitionConfigurator:
         required_groups.add(series_configuration.winner_rank_from_configuration)
         required_groups.add(series_configuration.loser_rank_from_configuration)
 
-        required_groups.remove(None)
-
         for gc in required_groups:
             CompetitionConfigurator.create_competition_group(gc, current_groups, sub_competition.competition)
 
@@ -176,14 +175,17 @@ class CompetitionConfigurator:
 
     # TODO: we're assuming the group is there
     @staticmethod
-    def get_group_from_list(group_name, group_list):
-        found_list = [g for g in group_list if g.name == group_name]
-        if found_list is None or len(found_list) == 0:
-            raise DomainError(f"Group {group_name} was not found.  Need to create group before calling this.")
-        elif len(found_list) > 1:
-            raise DomainError(f"{group_name} has multiple {len(found_list)} entries.")
+    def get_group_from_list(group_config, group_list):
+        if group_config is not None:
+            found_list = [g for g in group_list if g.name == group_config.name]
+            if found_list is None or len(found_list) == 0:
+                raise DomainError(f"Group {group_config.name} was not found.  Need to create group before calling this.")
+            elif len(found_list) > 1:
+                raise DomainError(f"{group_config.name} has multiple {len(found_list)} entries.")
+            else:
+                return [g for g in group_list if g.name == group_config.name][0]
         else:
-            return [g for g in group_list if g.name == group_name][0]
+            return None
 
     @staticmethod
     def processes_series_by_wins_configuration(series_configuration, current_groups, sub_competition):
@@ -194,19 +196,19 @@ class CompetitionConfigurator:
         series = SeriesByWins(sub_competition, series_configuration.name, series_configuration.series_round,
                               None, None, 0, 0, series_rules,
                               CompetitionConfigurator.get_group_from_list(
-                                  series_configuration.home_team_group_configuration.name, current_groups),
+                                  series_configuration.home_team_group_configuration, current_groups),
                               series_configuration.home_team_value,
                               CompetitionConfigurator.get_group_from_list(
-                                  series_configuration.away_team_group_configuration.name, current_groups),
+                                  series_configuration.away_team_group_configuration, current_groups),
                               series_configuration.away_team_value,
                               CompetitionConfigurator.get_group_from_list(
-                                  series_configuration.winner_group_configuration.name, current_groups),
+                                  series_configuration.winner_group_configuration, current_groups),
                               CompetitionConfigurator.get_group_from_list(
-                                  series_configuration.winner_rank_from_configuration.name, current_groups),
+                                  series_configuration.winner_rank_from_configuration, current_groups),
                               CompetitionConfigurator.get_group_from_list(
-                                  series_configuration.loser_group_configuration.name, current_groups),
+                                  series_configuration.loser_group_configuration, current_groups),
                               CompetitionConfigurator.get_group_from_list(
-                                  series_configuration.loser_rank_from_configuration.name, current_groups),
+                                  series_configuration.loser_rank_from_configuration, current_groups),
                               False, False)
 
         return series
--- tests/teams/domain/competition/playoff_config_by_example.py
+++ tests/teams/domain/competition/playoff_config_by_example.py
@@ -1,9 +1,18 @@
+import random
+
+from teams.ConsoleUI.views.playoff_views import SeriesView
 from teams.domain.comp_configorator import CompetitionConfigurator
+from teams.domain.competition import CompetitionTeam
 from teams.domain.competition_configuration import CompetitionConfiguration, RankingGroupConfiguration, \
     SubCompetitionConfiguration, SeriesConfiguration
 from teams.domain.game import GameRules
+from teams.domain.scheduler import Scheduler
+from teams.domain.series import SeriesByGoals, SeriesByWins
 from teams.domain.series_rules import SeriesRules, SeriesByWinsRules
 from teams.domain.team import Team
+from teams.services.game_service import GameService
+from teams.services.view_models.playoff_view_models import SeriesViewModel
+from teams.services.view_models.team_view_models import TeamViewModel
 
 toronto = Team("Toronto", 5, True)
 montreal = Team("Montreal", 5, True)
@@ -18,6 +27,8 @@ saskatoon = Team("Saskatoon", 5, True)
 hamilton = Team("Hamilton", 5, True)
 halifax = Team("Halifax", 5, True)
 
+teams = [toronto, montreal, ottawa, quebec_city, calgary, edmonton, vancouver, winnipeg, victoria, saskatoon, hamilton, halifax]
+
 playoff_game_rules = GameRules("Playoff Rules", False)
 series_rules = SeriesByWinsRules("Best of 7", 4, playoff_game_rules, [0, 0, 1, 1, 0, 1, 0])
 
@@ -27,7 +38,7 @@ playoff_config = SubCompetitionConfiguration("Playoff", competition_config, 1, S
 competition_config.sub_competitions.append(playoff_config)
 
 # seeding group
-league = RankingGroupConfiguration("League", playoff_config, None, 1, 1, None)
+league_config = RankingGroupConfiguration("League", playoff_config, None, 1, 1, None)
 
 # playoff groups
 r1_winners = RankingGroupConfiguration("R1 Winners", playoff_config, None, 1, 1, None)
@@ -35,26 +46,82 @@ r2_winners = RankingGroupConfiguration("R2 Winners", playoff_config, None, 1, 1,
 champion = RankingGroupConfiguration("Champion", playoff_config, None, 1, 1, None)
 runner_up = RankingGroupConfiguration("Runner Up", playoff_config, None, 1, 1, None)
 
-group_configs = [league, r1_winners, r2_winners, champion, runner_up]
+group_configs = [league_config, r1_winners, r2_winners, champion, runner_up]
 
 # round 1
-r1s1 = SeriesConfiguration("R1S1", 1, playoff_config, league, 1, league, 8, series_rules, r1_winners, league, None, None, 1, None)
-r1s2 = SeriesConfiguration("R1S2", 1, playoff_config, league, 2, league, 7, series_rules, r1_winners, league, None, None, 1, None)
-r1s3 = SeriesConfiguration("R1S3", 1, playoff_config, league, 3, league, 6, series_rules, r1_winners, league, None, None, 1, None)
-r1s4 = SeriesConfiguration("R1S4", 1, playoff_config, league, 4, league, 5, series_rules, r1_winners, league, None, None, 1, None)
+r1s1 = SeriesConfiguration("R1S1", 1, playoff_config, league_config, 1, league_config, 8, series_rules, r1_winners, league_config, None, None, 1, None)
+r1s2 = SeriesConfiguration("R1S2", 1, playoff_config, league_config, 2, league_config, 7, series_rules, r1_winners, league_config, None, None, 1, None)
+r1s3 = SeriesConfiguration("R1S3", 1, playoff_config, league_config, 3, league_config, 6, series_rules, r1_winners, league_config, None, None, 1, None)
+r1s4 = SeriesConfiguration("R1S4", 1, playoff_config, league_config, 4, league_config, 5, series_rules, r1_winners, league_config, None, None, 1, None)
 # round 2
-r2s1 = SeriesConfiguration("R2S1", 2, playoff_config, r1_winners, 1, r1_winners, 4, series_rules, r2_winners, league, None, None, 1, None)
-r2s2 = SeriesConfiguration("R2S2", 2, playoff_config, r1_winners, 2, r1_winners, 3, series_rules, r2_winners, league, None, None, 1, None)
+r2s1 = SeriesConfiguration("R2S1", 2, playoff_config, r1_winners, 1, r1_winners, 4, series_rules, r2_winners, league_config, None, None, 1, None)
+r2s2 = SeriesConfiguration("R2S2", 2, playoff_config, r1_winners, 2, r1_winners, 3, series_rules, r2_winners, league_config, None, None, 1, None)
 # round 3
-r3s1 = SeriesConfiguration("R3S1", 3, playoff_config, r2_winners, 1, r2_winners, 4, series_rules, champion, league, runner_up, league, 1, None)
+r3s1 = SeriesConfiguration("R3S1", 3, playoff_config, r2_winners, 1, r2_winners, 2, series_rules, champion, league_config, runner_up, league_config, 1, None)
 
 series_configs = [r1s1, r1s2, r1s3, r1s4, r2s2, r2s1, r3s1]
 
 current_groups = []
 competition = CompetitionConfigurator.create_competition(competition_config, 1)
 playoff = CompetitionConfigurator.create_sub_competition(playoff_config, competition)
-CompetitionConfigurator.create_competition_group(league, current_groups, competition)
+league = CompetitionConfigurator.create_competition_group(league_config, current_groups, competition)
 
 for series_config in series_configs:
     CompetitionConfigurator.process_series_configuration(series_config, current_groups, playoff)
+
+r = random
+
+comp_teams = [CompetitionTeam(competition, t) for t in teams]
+r.shuffle(comp_teams)
+
+count = 1
+for team in comp_teams:
+    league.add_team_to_group(team, count)
+    count += 1
+
+for league_rank in league.rankings:
+    print(f'{league_rank.rank}. {league_rank.team.name}')
+
+current_day = 1
+days = {}
+games = []
+#  setup first round of playoff
+while not playoff.is_complete():
+    print(f'Current Round: {playoff.current_round}')
+    while not playoff.is_round_complete(playoff.current_round):
+        if not playoff.is_round_setup(playoff.current_round):
+            playoff.setup_round(playoff.current_round)
+        # print(loop)
+        new_games = playoff.create_new_games(games)
+        Scheduler.add_games_to_schedule(new_games, days, r, current_day)
+        games.extend(new_games)
+        for g in days[current_day]:
+            g.play(r)
+            model = GameService.game_to_vm(g)
+            playoff.process_game(g)
+            # print(GameView.get_basic_view(model))
+        game_day_view_model = GameService.games_to_game_day_view(days[current_day])
+        #print(GameDayView.get_view(game_day_view_model))
+        current_day += 1
+
+    for s in [ps for ps in playoff.series if ps.series_round == playoff.current_round]:
+        home_value = -1
+        away_value = -1
+        if isinstance(s, SeriesByGoals):
+            home_value = s.home_goals
+            away_value = s.away_goals
+        elif isinstance(s, SeriesByWins):
+            home_value = s.home_wins
+            away_value = s.away_wins
+
+        view_model = SeriesViewModel(s.name, s.sub_competition.competition.year, s.series_round, None,
+                                     TeamViewModel(s.home_team.oid, s.home_team.name, s.home_team.skill, True),
+                                     home_value,
+                                     TeamViewModel(s.away_team.oid, s.away_team.name, s.away_team.skill, True),
+                                     away_value,
+                                     "Done")
+        print(SeriesView.get_basic_series_view(view_model))
+    # post process round and series
+    playoff.post_process_round(playoff.current_round)
+    playoff.current_round += 1
 
