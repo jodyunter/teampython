14fa2e3d0fd8f81a9d46ed8e17efaf8dddfca95f
jodyu <jody_unterschutz@tcenergy.com>
2021-03-12 14:29:15 -0700

    trying to start data layer

---
+++ teams/data/dto/dto_sub_competition.py
@@ -0,0 +1,21 @@
+from abc import ABC
+
+from teams.data.dto.dto_base import Base
+from teams.domain.sub_competition import SubCompetition
+
+
+class SubCompetitionDTO(ABC, Base, SubCompetition):
+
+    def __init__(self, sub_competition):
+
+        SubCompetition.__init__(self,
+                                sub_competition.name,
+                                sub_competition.sub_competition_type,
+                                sub_competition.competition,
+                                sub_competition.groups,
+                                sub_competition.order,
+                                sub_competition.setup,
+                                sub_competition.started,
+                                sub_competition.finished,
+                                sub_competition.post_processed,
+                                sub_competition.oid)
