e7f2add6056ecf1903464568ea48cecaf869da1e
jodyu <jody_unterschutz@tcenergy.com>
2022-01-14 13:03:52 -0700

    next

--- tests/teams/domain/competition/testing_sample_data.py
+++ tests/teams/domain/competition/testing_sample_data.py
@@ -7,6 +7,7 @@ from teams.data.repo.team_repository import TeamRepository
 from teams.domain import CompetitionConfiguration, TableSubCompetitionConfiguration, CompetitionTeamConfiguration
 from teams.domain.competition_group_configuration import RankingGroupConfiguration
 from teams.domain.team import Team
+from teams.services.team_service import TeamService
 
 connection = "sqlite:///:memory:"
 Database.init_db(connection)
@@ -14,41 +15,20 @@ session = Database.get_session()
 Database.clean_up_database(session)
 session.commit()
 
-toronto = Team("Toronto", 5, True)
-montreal = Team("Montreal", 5, True)
-ottawa = Team("Ottawa", 5, True)
-quebec_city = Team("Quebec City", 5, True)
-calgary = Team("Calgary", 5, True)
-edmonton = Team("Edmonton", 5, True)
-vancouver = Team("Vancouver", 5, True)
-winnipeg = Team("Winnipeg", 5, True)
-victoria = Team("Victoria", 5, True)
-saskatoon = Team("Saskatoon", 5, True)
-hamilton = Team("Hamilton", 5, True)
-halifax = Team("Halifax", 5, True)
-boston = Team("Boston", 5, True)
-new_york = Team("New York", 5, True)
-detroit = Team("Detroit", 5, True)
-chicago = Team("Chicago", 5, True)
-seattle = Team("Seattle", 5, True)
-minnesota = Team("Minnesota", 5, True)
-colorado = Team("Colorado", 5, True)
-san_jose = Team("San Jose", 5, True)
-los_angelas = Team("Los Angelas", 5, True)
-pittsburgh = Team("Pittsburgh", 5, True)
+team_data = {
+    "Toronto": [5, True],
+    "Montreal": [5, True],
+    "Ottawa": [5, True],
+    "Winnipeg": [5, True],
+    "Calgary": [5, True],
+    "Vancouver": [5, True]
+}
+skill = 0
+active = 1
 
-teams = [toronto, montreal, ottawa, quebec_city, calgary, edmonton, vancouver, winnipeg, victoria, saskatoon,
-         hamilton, halifax, boston, new_york, detroit, chicago, seattle, minnesota, colorado, san_jose, los_angelas,
-         pittsburgh]
-western_teams = [calgary, edmonton, vancouver, winnipeg, victoria, saskatoon, seattle, minnesota, colorado, san_jose,
-                 los_angelas]
-eastern_teams = [toronto, montreal, ottawa, quebec_city, hamilton, halifax, boston, new_york, detroit, chicago,
-                 pittsburgh]
-pacific_teams = [vancouver, victoria, seattle, san_jose, colorado, los_angelas]
-central_teams = [edmonton, calgary, winnipeg, saskatoon, minnesota]
-atlantic_teams = [montreal, quebec_city, halifax, boston, new_york, pittsburgh]
-north_teams = [toronto, ottawa, hamilton, detroit, chicago]
+teams = ["Toronto", "Montreal", "Ottawa", "Winnipeg", "Calgary", "Vancouver"]
 
+team_service = TeamService()
 team_repo = TeamRepository()
 competition_config_repo = CompetitionConfigurationRepository()
 table_config_repo = TableSubCompetitionConfigurationRepository()
@@ -56,8 +36,8 @@ ranking_group_config_repo = RankingGroupConfigurationRepository()
 competition_team_config_repo = CompetitionTeamConfigurationRepository()
 
 # create all the teams
-[team_repo.add(team, session) for team in teams]
-session.commit()
+for t in team_data:
+    team_service.create(t, team_data[t][skill], team_data[t][active])
 
 # create the competition configuration
 competition_config = CompetitionConfiguration("League Configuration", [], [], 1, 1, None)
@@ -78,5 +58,5 @@ session.commit()
 # premier has all
 [competition_team_config_repo.add(
     CompetitionTeamConfiguration(team, competition_config, premier_division_config, 1, None), session)
- for team in teams]
+    for team in [team_repo.get_by_name(name, session) for name in teams]]
 session.commit()
