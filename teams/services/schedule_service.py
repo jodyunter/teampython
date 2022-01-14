5578a7f880b4fe71aa24c74572b491491213eb7d
jodyu <jody_unterschutz@tcenergy.com>
2022-01-14 15:06:02 -0700

    next

--- teams/services/schedule_service.py
+++ teams/services/schedule_service.py
@@ -1,9 +1,30 @@
+from teams.data.repo.game_repository import GameRepository
 from teams.services.base_service import BaseService
 
 
 class ScheduleService(BaseService):
+    game_repo = GameRepository()
 
-    @staticmethod
-    def add_game_to_schedule(game, starting_day):
-        # using queries, find the first day on or after the starting day, where the two teams do not play
-        pass
+    def get_repo(self):
+        return self.game_repo
+
+    def add_game_to_schedule(self, game, starting_day, session=None):
+        commit = session is None
+        session = self.get_session(session)
+        ending_day = 99999
+        days = self.get_repo().get_list_days_teams_play_on(game.year, starting_day, ending_day, game, session)
+
+        days.sort()
+
+        added = False
+        day = starting_day
+        while not added:
+            if not day in days:
+                game.day = day
+                added = True
+            else:
+                day += 1
+
+        self.get_repo().add(game)
+
+        self.commit(session, commit)
