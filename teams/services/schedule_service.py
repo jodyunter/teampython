660bbae6092e7d227ec95486bf2f001f55b62762
jodyu <jody_unterschutz@tcenergy.com>
2022-01-18 12:42:47 -0700

    Making more re-usable scheduling methods

--- teams/services/schedule_service.py
+++ teams/services/schedule_service.py
@@ -9,16 +9,26 @@ class ScheduleService(BaseService):
         return self.game_repo
 
     # this is using a domain object in the game
-    def add_game_to_schedule(self, game_view_model, starting_day, ending_day, session=None):
+    def add_game_to_schedule(self, year, game_view_model, starting_day, ending_day, session=None):
         commit = session is None
         session = self.get_session(session)
 
+        self.add_games_as_day(year, [game_view_model], starting_day, ending_day, session)
+
+        self.commit(session, commit)
+
+    def add_games_as_day(self, year, game_view_model_list, starting_day, ending_day, session=None):
+        commit = session is None
+        session = self.get_session(session)
+
+        team_id_list = [game_vm.home_id for game_vm in game_view_model_list]
+        team_id_list.append([game_vm.away_id for game_vm in game_view_model_list])
+
         if ending_day is None:
             ending_day = 99999
-            
-        days = self.get_repo().get_invalid_schedule_days(game_view_model.year, starting_day, ending_day,
-                                                         [game_view_model.home_id,
-                                                          game_view_model.away_id], session)
+
+        days = self.get_repo().get_invalid_schedule_days(year, starting_day, ending_day,
+                                                         team_id_list, session)
 
         days.sort()
 
@@ -26,11 +36,13 @@ class ScheduleService(BaseService):
         day = starting_day
         while not added:
             if day not in days:
-                game_view_model.day = day
+                for game_vm in game_view_model_list:
+                    game_vm.day = day
                 added = True
             else:
                 day += 1
 
-        self.get_repo().add(game_view_model, session)
+        for game_vm in game_view_model_list:
+            self.get_repo().add(game_vm, session)
 
         self.commit(session, commit)
